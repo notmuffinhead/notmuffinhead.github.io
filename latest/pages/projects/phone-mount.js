0b57944a28e9b0de005895a083a0ac66a437fde3
Maggie <maggiewu801@gmail.com>
2025-08-23 17:36:06 -0400

    fixed apostrophe errors by wrapping text in curly braces

--- latest/pages/projects/phone-mount.js
+++ latest/pages/projects/phone-mount.js
@@ -9,42 +9,44 @@ export default function PhoneMount() {
         <Hero showBackButton={true} />
         <div className={styles.projectDetail}> 
 
-            <h2>Phone Mount</h2> 
+            <h2>{"Phone Mount"}</h2> 
 
             <div className={styles.projectContent}>
             <section>
-            <h2>Inspiration?</h2>
+            <h2>{"Inspiration?"}</h2>
             <p>
+                {`
                 I've always wanted a hands-free way to record POV mountain biking clips, but didn't want to buy a GoPro or separate camera.
                 However, not many suitable phone mounts exist; chest mounts are large and expensive, and helmet mounts seem pretty sketchy. 
                 There exist bite mounts for smaller action cameras, but having one for something as heavy as a modern phone wouldn't be too smart.
                 One ride, I had been drinking Gatorade and occasionally had to hold the bottle with just my mouth while my hands were occupied with steering. 
                 Random ideas flowed into my mind for the rest of the ride, and not too long after, I had ... this.
                 As a result, I don't recommend making this for utility; it's just the aftermath of an intrusive thought taken seriously. 
+                `}
             </p>
             </section>
 
             <section>
-            <h2>A Video I Took with It</h2> 
+            <h2>{"A Video I Took with It"}</h2> 
             <p>
-                Insert video here somehow
+                {"Insert video here somehow"}
             </p>
             </section>
 
             <section>
-            <h2>Materials</h2>
+            <h2>{"Materials"}</h2>
             <ul>
-                <li>Gatorade bottle </li>
-                <li>3 screws </li>
-                <li>Velcro </li>
-                <li>3D printing supplies </li>
+                <li>{"Gatorade bottle"}</li>
+                <li>{"3 screws"}</li>
+                <li>{"Velcro"}</li>
+                <li>{"3D printing supplies"}</li>
             </ul> 
             </section>
 
             <section>
-            <h2>The Process</h2>
+            <h2>{"The Process"}</h2>
             <p>
-                Maybe put down some stl files idk 
+                {"Maybe put down some stl files idk"}
             </p>
             </section>
             </div>
--- latest/pages/projects/wifi-lab.js
+++ latest/pages/projects/wifi-lab.js
@@ -14,26 +14,29 @@ export default function WifiLab() {
 
         <div className={styles.projectContent}>
           <section>
-            <h2>Overview</h2>
+            <h2>{"Overview"}</h2>
             <p>
-              talk a bit more here 
-              This antenna achieved max distance (22m) at the New York State Science Olympiad Tournament in 2023. 
+              {` 
+              Wifi lab is a Science Olympiad event where you construct a 2.4GHz antenna before the tournament, 
+              and take a written test on the principles of EM wave propagation. 
+              This antenna achieved max transmission distance (22m) at the New York State Science Olympiad Tournament in 2023, and I ended up getting Third Place.
+              `}
             </p>
           </section>
 
           <section>
-            <h2>Materials</h2>
+            <h2>{"Materials"}</h2>
             <ul>
-              <li>12 AWG copper wire</li>
-              <li>Female BNC panel mount connector </li>
-              <li>BNC male to SMA male connector</li>
-              <li>Electrical tape</li>
-              <li>0.8mm thick aluminum reflector </li>
+              <li>{"12 AWG copper wire"}</li>
+              <li>{"Female BNC panel mount connector"}</li>
+              <li>{"BNC male to SMA male connector"}</li>
+              <li>{"Electrical tape"}</li>
+              <li>{"0.8mm thick aluminum reflector"}</li>
             </ul> 
           </section>
 
           <section>
-            <h2>Labeled Diagram</h2>
+            <h2>{"Labeled Diagram"}</h2>
             <Image
               src="/images/wifi-lab-diagram.jpg"
               width={600}
@@ -42,10 +45,24 @@ export default function WifiLab() {
           </section>
 
           <section>
-            <h2>The Process</h2>
+            <h2>{"The Process"}</h2>
             <p>
-              Section not done yet but planning to write about the other antennas i tried to make and why this one ended up being the best,
-              remember to mention andrew mcneil 
+              {"I also built and tested the following antennas (which didn't perform as well):"} 
+              <ul>
+                <li>{"Cloverleaf antenna"}</li>
+                <li>{"Single biquad antenna"}</li>
+                <li>{"Double biquad antenna (using thinner wire)"}</li>
+                <li>{"Parabolic double biquad antenna"}</li> 
+              </ul>
+              {"Andrew McNeil's YouTube videos were really helpful for learning to build these antennas."} 
+            </p>
+          </section>
+
+          <section>
+            <h2>{"Study Guide"}</h2>
+            <p>
+              {"The binder that my partner and I put together for the test-taking portion of the event!"} 
+              {"(insert file here)"}
             </p>
           </section>
         </div>
